52a0112bf5f592542818e2c2877df2ad7629bd4c
Alexis <alexis@parfaitementweb.com>
2024-01-16 09:37:28 +0100

    Github Worlflow update.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,8 +8,12 @@ use Filament\Actions\ActionsServiceProvider;
 use Filament\FilamentServiceProvider;
 use Filament\Forms\FormsServiceProvider;
 use Filament\Infolists\InfolistsServiceProvider;
+use Filament\Notifications\NotificationsServiceProvider;
+use Filament\SpatieLaravelSettingsPluginServiceProvider;
+use Filament\SpatieLaravelTranslatablePluginServiceProvider;
 use Filament\Support\SupportServiceProvider;
 use Filament\Tables\TablesServiceProvider;
+use Filament\Widgets\WidgetsServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
@@ -38,8 +42,12 @@ class TestCase extends Orchestra
             FormsServiceProvider::class,
             InfolistsServiceProvider::class,
             LivewireServiceProvider::class,
+            NotificationsServiceProvider::class,
+            SpatieLaravelSettingsPluginServiceProvider::class,
+            SpatieLaravelTranslatablePluginServiceProvider::class,
             SupportServiceProvider::class,
             TablesServiceProvider::class,
+            WidgetsServiceProvider::class,
             FilamentCountryFieldServiceProvider::class,
         ];
     }
